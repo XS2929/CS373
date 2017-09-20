e46ea686da51507288137d40e44323a4c82dc21c
Glenn Downing <downing@cs.utexas.edu>
2017-09-19 22:41:47 -0500

    another commit

---
+++ notes/09-18.py
@@ -0,0 +1,13 @@
+# -----------
+# Mon, 18 Sep
+# -----------
+
+"""
+finish the overview of types
+
+Edward Doan
+Google
+
+Platform       as a Service (Paas): App Engine
+Infrastructure as a Service (Iaas): Compute Engine
+"""
