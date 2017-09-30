59a6e73a730f15eed17d7f7513470eac5e972fd9
Glenn Downing <downing@cs.utexas.edu>
2017-09-29 21:19:04 -0500

    another commit

--- notes/09-29.py
+++ notes/09-29.py
@@ -3,6 +3,51 @@
 # -----------
 
 """
-reduce()
 more iteration
+iterable vs iterator
+comprehensions
 """
+
+a = [2, 3, 4]
+a[1] = 5      # l-value
+
+a = (2, 3, 4)
+a[1] = 5      # not ok; r-value
+
+x = range(...)
+x[1] = 5      # not ok; r-value
+
+a = [2, 3, 5]
+print(type(a)) # list, iterable, not an iterator
+
+p = iter(a)
+print(type(p)) # list iterator, iterator and iterable!
+
+assert(a is p) # false; a is a list; p is a list iterator
+
+q = iter(p)
+assert(p is q) # true; iter(p) returns itself
+
+for v in a : # new list iterator
+    print(v) # 2 3 4
+
+for v in a : # new list iterator
+    print(v) # 2 3 4; does not get exhausted
+
+for v in p : # iter(p) returns p
+    print(v) # 2 3 4
+
+for v in p : # old iterator
+    print(v) # <nothing>; exhausted
+
+a = <iterable object>
+p = iter(a)
+
+# special processing for the first 3 elements
+f(next(p))
+f(next(p))
+f(next(p))
+
+# different processing for the remaining elements
+for v in p :
+    g(v)
