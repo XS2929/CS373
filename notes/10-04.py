ec6aa01e539e0b7d0103d473b4a1b811c818cf34
Glenn Downing <downing@cs.utexas.edu>
2017-10-04 17:29:00 -0500

    another commit

--- notes/10-04.py
+++ notes/10-04.py
@@ -1,8 +1,27 @@
 # -----------
-# Mon,  2 Oct
+# Wed,  4 Oct
 # -----------
 
 """
-yield
+finish comprehensions
 map()
 """
+
+a = [2, 3, 4]
+a += [5]      # inline list concatenation
+
+a = {2, 3, 4}
+a |= {2}      # inline set union
+
+x = f(...)    # function? constructor?
+
+class my_map :
+    def __init__ (self, uf, a) :
+        self.p  = iter(a)
+        self.uf = uf
+
+    def __next__ (self) :
+        return self.uf(next(self.p))
+
+    def __iter__ (self) :
+        return self
