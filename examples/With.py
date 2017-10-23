44d189f3f6b1363bb817450f9f2735471a0b9dbe
Glenn Downing <downing@cs.utexas.edu>
2017-10-22 21:10:28 -0500

    another commit

---
+++ examples/With.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+# pylint: disable = too-few-public-methods
+
+# -------
+# With.py
+# -------
+
+def test1 () :
+    f = open("With.py")
+    try :
+        assert f.readline() == "#!/usr/bin/env python3\n"
+    finally :
+        print("closing file")
+        f.close()
+
+class my_file :
+    def __init__ (self, s) :
+        self.s = s
+        self.f = None
+
+    def __enter__ (self) :
+        self.f = open(self.s)
+        return self.f
+
+    def __exit__ (self, *a) :
+        print("exiting my_file")
+        self.f.close()
+
+def test2 () :
+    x = my_file("With.py")
+    f = x.__enter__()
+    try :
+        assert f.readline() == "#!/usr/bin/env python3\n"
+    finally :
+        x.__exit__()
+
+def test3 () :
+    with my_file("With.py") as f :
+        assert f.readline() == "#!/usr/bin/env python3\n"
+
+def test4 () :
+    with open("With.py") as f :
+        assert f.readline() == "#!/usr/bin/env python3\n"
+
+if __name__ == "__main__" : # pragma: no cover
+    print("With.py")
+    test1()
+    test2()
+    test3()
+    test4()
+    print("Done.")
--- makefile
+++ makefile
@@ -133,6 +133,7 @@ sync:
     --include "ThetaJoin2.py"                \
     --include "NaturalJoin1.py"              \
     --include "NaturalJoin2.py"              \
+    --include "With.py"                      \
     --exclude "*"                            \
     ../../examples/python/ examples
 	@rsync -r -t -u -v --delete             \
