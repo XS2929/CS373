40871677e041d435ec55d5ade4d04bade6ded7b4
Glenn Downing <downing@cs.utexas.edu>
2017-11-07 07:58:45 -0600

    another commit

---
+++ notes/11-06.sql
@@ -0,0 +1,20 @@
+select sName from Student
+where sName in
+(select cName from College)
+
+select sID from Student
+where sID not in
+(select sID from Apply)
+
+select *
+    from Student as R
+    where in
+    (select
+
+select count(*)
+    from Apply
+    group by cName;
+
+select count(distinct sID)
+    from Apply
+    group by cName;
