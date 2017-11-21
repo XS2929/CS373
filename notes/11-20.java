9f32876c12e09fd14335abfd2415fad450566b67
Glenn Downing <downing@cs.utexas.edu>
2017-11-20 18:52:48 -0600

    another commit

--- notes/11-20.java
+++ notes/11-20.java
@@ -2,3 +2,11 @@
 // Mon, 20 Nov
 // -----------
 
+abstract class A {
+    public void f (int) {}
+    public final void g (int) {}
+    public abstract void h (int) {}}
+
+class B extends A {
+    @override
+    public void f (int) {}}
